7fcadc30d1621b7ffdbad508ef81efff88d4979b
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2022-01-20 21:16:21 -0600

    init

---
+++ src/components/Social.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const SocialShare = [
+  {
+    iconName: "github",
+    link: "https://www.facebook.com/",
+  },
+];
+const Social = () => {
+  return (
+    <>
+      <ul className="social">
+        {SocialShare.map((val, i) => (
+          <li key={i}>
+            <a href={`${val.link}`} target="_blank" rel="noreferrer">
+              <img
+                className="svg"
+                src={`/assets/img/svg/social/${val.iconName}.svg`}
+                alt="social"
+              ></img>
+            </a>
+          </li>
+        ))}
+      </ul>
+      {/* END SOCIAL */}
+    </>
+  );
+};
+
+export default Social;
