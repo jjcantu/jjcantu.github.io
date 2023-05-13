b0332d6e1cc981daf3478f1111ab7fe3f75262e3
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2023-05-13 06:08:11 -0500

    git init

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+export { default as About } from './About';
+export {
+  BallCanvas,
+  ComputersCanvas,
+  EarthCanvas,
+  StarsCanvas,
+} from './canvas';
+export { default as Contact } from './Contact';
+export { default as Experience } from './Experience';
+export { default as Feedbacks } from './Feedbacks';
+export { default as Hero } from './Hero';
+export { default as Navbar } from './Navbar';
+export { default as Tech } from './Tech';
+export { default as Works } from './Works';
